008b7c7838267b3244bb7e4a56c5d97cf058ab16
DieroKreator <qtechwired@gmail.com>
2024-10-28 14:10:38 -0300

    first commit

---
+++ src/test/java/TicketTest.java
@@ -0,0 +1,44 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TicketTest {
+    
+    private WebDriver driver;
+
+    @BeforeEach
+    public void setUp() {
+        driver = new ChromeDriver(); 
+    }
+
+    @AfterEach
+    public void tearDown() {
+        driver.quit();
+    }
+
+    @Test
+    public void buyTicket() {
+        driver.get("https://blazedemo.com");
+        driver.manage().window().maximize();
+
+        // combo origem
+        driver.findElement(By.name("fromPort")); // clica no combo
+        {
+            WebElement dropdown = driver.findElement(By.name("fromPort"));
+            dropdown.findElement(By.xpath("//option[. = 'São Paolo']")).click();
+        }
+
+        // combo Destino
+        {
+            WebElement dropdown = driver.findElement(By.name("toPort"))
+            dropdown.click(); // clica no combo
+            dropdown.findElement(By.xpath("//option[. = 'Cairo']")).click();
+        }
+
+        driver.findElement(By.cssSelector("input.btn.btn-primary"));
+    }
+}
